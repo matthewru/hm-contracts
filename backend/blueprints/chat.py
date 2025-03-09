72b84e68708f76223677a66679024204c04be268
matthewru <matthewru2005@gmail.com>
2025-03-08 23:24:40 -0800

    integrated the database with the chat updating

--- backend/blueprints/chat.py
+++ backend/blueprints/chat.py
@@ -4,7 +4,8 @@ import subprocess
 from flask import Blueprint, render_template, request, jsonify
 from gemini.modify import modify_latex
 from scripts.make4ht import convert_latex_to_html
-# from db_helpers import get_user_by_id, update_user_documents
+from db_helpers import db
+from bson import ObjectId
 
 chat_bp = Blueprint('chat_bp', __name__)
 
@@ -20,13 +21,21 @@ def start_chat():
 
     max_attempts = 3
     response_latex = context
+    
+    user_object = db['users'].find_one({
+                "documents.latexcontent": response_latex
+            })
 
     for attempt in range(max_attempts):
         try:
             response_latex, response_chat = modify_latex(context, focus, message)
 
-            print(response_latex)
-            print(response_chat)
+            # print(response_latex)
+            # print(response_chat)
+
+            
+            
+            
 
             with tempfile.TemporaryDirectory() as tmpdir:
                 tex_file = os.path.join(tmpdir, "contract.tex")
@@ -44,9 +53,43 @@ def start_chat():
 
                     # doc_payload = {"content": html_content, 
                     #                "doctype": "",
-                    #                "client": ""} # TODO: MATTHEW
+                    #                "client": ""} 
+                    # 
+                    # # TODO: MATTHEW
 
                     # update_user_documents(user_id, doc_payload)
+                    
+                    if user_object:
+                        user_id = user_object.get("_id")
+                        
+                        
+                        # Find the specific document in the user's documents array
+                        document_index = None
+                        for i, doc in enumerate(user_object.get("documents", [])):
+                            if doc.get("latexcontent") == context:
+                                document_index = i
+                                break
+                        
+                        if document_index is not None:
+                            # Update the specific document with modified content
+                            update_result = db['users'].update_one(
+                                            {
+                                                "_id": ObjectId(user_id),
+                                                "documents.latexcontent": context
+                                            },
+                                            {
+                                                "$set": {
+                                                    "documents.$.latexcontent": response_latex,
+                                                    "documents.$.htmlcontent": response_html
+                                                }
+                                            }
+                                        )
+
+                            print(update_result)
+                        else:
+                            print("User not found for the given LaTeX content")
+                    
+                    
                     return jsonify({
                         "latex": response_latex,
                         "html": response_html,
